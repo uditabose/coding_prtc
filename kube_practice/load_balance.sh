1266cda6301a1268726fb60cb5e39af4a731a86d
Udita Bose <ub298@nyu.edu>
2018-04-30 22:21:42 -0400

    kube : module 6 update app

--- kube_practice/load_balance.sh
+++ kube_practice/load_balance.sh
@@ -4,7 +4,7 @@ source colors.sh
 
 log_header "balance 'em out"
 
-export NODE_PORT=$(kubectl get services/kubernetes-bootcamp -o go-tempe='{{(index .spec.ports 0).nodePort}}')
+export NODE_PORT=$(kubectl get services/kubernetes-bootcamp -o go-template --template '{{(index .spec.ports 0).nodePort}}')
 echo NODE_PORT=$NODE_PORT
 
 for (( i = 0; i < 10; i++ )); do
---
+++ kube_practice/update_app.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+ 
+source colors.sh
+
+log_header 'update the app'
+
+# see pods
+kubectl describe pods
+
+log_section 'updating image'
+# update image
+kubectl set image deployments/kubernetes-bootcamp kubernetes-bootcamp=jocatalin/kubernetes-bootcamp:v2
+
+# see again
+kubectl get pods
+
+# describe service
+kubectl describe services/kubernetes-bootcamp
+
+# the node port
+export NODE_PORT=$(kubectl get services/kubernetes-bootcamp -o go-template='{{(index .spec.ports 0).nodePort}}')
+echo NODE_PORT=$NODE_PORT
+
+curl $(minikube ip):$NODE_PORT
+
+log_section 'rolls out'
+# roll stuff out
+kubectl rollout status deployments/kubernetes-bootcamp
+
+# describe again
+kubectl describe pods
+
+log_section 'update image again'
+kubectl set image deployments/kubernetes-bootcamp kubernetes-bootcamp=gcr.io/google-samples/kubernetes-bootcamp:v10
+
+# see the goodies
+kubectl get deployments
+
+# see more
+kubectl describe pods
+
+log_section 'roll, roll, roll'
+# rolls out again
+kubectl rollout undo deployments/kubernetes-bootcamp
+
+# see again
+kubectl describe pods
