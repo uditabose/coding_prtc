d9edb75179d72eeb8748617c7e03b44e9841069d
Udita Bose <ub298@nyu.edu>
2018-06-30 16:54:04 -0400

    kube : updated

--- kube_practice/lfd259ckad/to-pdf.sh
+++ kube_practice/lfd259ckad/to-pdf.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-source colors.sh
+source ../common/colors.sh
 PDF_NAME="$1"
-
+PWD=`pwd`
 if [[ -z  "$PDF_NAME" ]]; then
     log_error "invalid PDF name"
     exit -1
 fi
 
-convert ~/Desktop/*.png -units PixelsPerInch -density 200 /Users/papa/spaces/workspace/Random/kube_practice/lfd259ckad/doc/$PDF_NAME.pdf
+convert ~/Desktop/*.png -units PixelsPerInch -density 200 "$PWD/doc/$PDF_NAME.pdf"
 
 mv -v ~/Desktop/*.png /tmp
 
